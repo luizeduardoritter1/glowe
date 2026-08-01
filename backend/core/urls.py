edb057d5102431ba4f7d3e73c8203b50761f3b14
luizeduardoritter1 <luizeduardoritter6@gmail.com>
2026-08-01 01:48:22 -0300

    feat: adiciona pagina detalhe do cliente

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('clientes/', views.lista_clientes, name='lista_clientes'),
+    path('clientes/<int:id>/', views.detalhe_cliente, name='detalhe_cliente'),
     path('agendamentos/', views.lista_agendamentos, name='lista_agendamentos')
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Cliente, Agendamento
 
 def lista_clientes(request):
     clientes = Cliente.objects.all()
     return render(request, 'core/lista_clientes.html', {'clientes': clientes})
+
+def detalhe_cliente(request, id):
+    cliente = get_object_or_404(Cliente, id=id)
+    return render(request, 'core/detalhe_cliente.html', {'cliente': cliente})
 
 def lista_agendamentos(request):
     agendamentos = Agendamento.objects.all()
